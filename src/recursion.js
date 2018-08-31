97715edecb41fad52bfb0fcf5e8470196b0e2b13
htlin2 <htlin2@gmail.com>
2018-08-31 11:24:20 +0800

    1 - 7

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,73 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  } else if (n < 0) {
+    return null;
+  }
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (Array.isArray(element)) {
+      sum += arraySum(element);
+    } else {
+      sum += element;
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  n = Math.abs(n);
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  }
+  return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (Math.sign(n) === -1) {
+    return n + 1 + sumBelow(n + 1);
+  }
+  return n - 1 + sumBelow(n - 1);
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y || x === y - 1 || x === y + 1) {
+    return [];
+  }
+  if (x < y) {
+    return [].concat(x + 1).concat(range(x + 1, y));
+  }
+  return [].concat(x - 1).concat(range(x - 1, y));
 };
 
 // 7. Compute the exponent of a number.
@@ -39,9 +81,15 @@ var range = function(x, y) {
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
-var exponent = function(base, exp) {
+var exponent = function (base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp > 0) {
+    return base * exponent(base, exp - 1);
+  }
+  return 1 / exponent(base, exp * -1);
 };
-
 // 8. Determine if a number is a power of two.
 // powerOfTwo(1); // true
 // powerOfTwo(16); // true
@@ -111,6 +159,25 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+  function fizzBuzzRecursive(n) {
+    var results = [];
+    if (n === 1) {
+      return '1';
+    } else {
+      if (n % 3 === 0 && n % 5 === 0) {
+        results.push('FizzBuzz');
+      } else if (n % 5 === 0) {
+        results.push('Buzz');
+      } else if (n % 3 === 0) {
+        results.push('Fizz');
+      } else {
+        results.push('' + n);
+      }
+      return results.concat(fizzBuzzRecursive(n - 1));
+    }
+  }
+
+  return fizzBuzzRecursive(n).reverse();
 };
 
 // 20. Count the occurence of a value in a list.
